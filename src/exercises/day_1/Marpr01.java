b8a4f6d0479e90a745a142469c9e8ef4c785a347
Paweł Wojtuń <panpawelw@protonmail.com>
2024-11-26 19:30:05 +0100

    Day 1 exercise 1

---
+++ src/exercises/day_1/Marpr01.java
@@ -0,0 +1,11 @@
+package exercises.day_1;
+
+public class Marpr01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello Coders Lab");
+
+        System.out.print("Hello Java");
+        System.out.print("Hello Coders Lab");
+    }
+}
