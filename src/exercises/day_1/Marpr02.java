61b884465f16c04c4bd2ab0ace68cee661aaa971
Paweł Wojtuń <panpawelw@protonmail.com>
2024-11-27 19:52:21 +0100

    Day 1 exercise 2

---
+++ src/exercises/day_1/Marpr02.java
@@ -0,0 +1,8 @@
+package exercises.day_1;
+
+public class Marpr02 {
+    public static void main(String[] args) {
+        String name = "Paweł";
+        System.out.println("Witaj, " + name);
+    }
+}
