285ec7949a50c335b5c4a0cff80a9b4611ace5a0
Paweł Wojtuń <panpawelw@protonmail.com>
2024-11-28 23:50:13 +0100

    Day 1 exercise 3

---
+++ src/exercises/day_1/Marpr03.java
@@ -0,0 +1,17 @@
+package exercises.day_1;
+
+public class Marpr03 {
+    public static void main(String[] args) {
+        int x = 10;
+        char c = 'O';
+        long y = 543263265;
+        double z = 44.999;
+        boolean b = true;
+
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(b);
+    }
+}
