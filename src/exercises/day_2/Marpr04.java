c6ac8dfe59fd79056fa558acfee832340285b18a
Paweł Wojtuń <panpawelw@protonmail.com>
2024-11-30 23:46:54 +0100

    Day 2 exercise 1

---
+++ src/exercises/day_2/Marpr04.java
@@ -0,0 +1,12 @@
+package exercises.day_2;
+
+import java.util.Scanner;
+
+public class Marpr04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoje imię:");
+        String name = scanner.nextLine();
+        System.out.println("Cześć, " + name + "!");
+    }
+}
