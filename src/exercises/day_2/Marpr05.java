1d5f3b00709b00da62f7a21a110627c8c2deb3ca
Paweł Wojtuń <panpawelw@protonmail.com>
2024-12-01 23:49:33 +0100

    Day 2 exercise 2

---
+++ src/exercises/day_2/Marpr05.java
@@ -0,0 +1,13 @@
+package exercises.day_2;
+
+import java.util.Scanner;
+
+public class Marpr05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swój wiek:");
+        int age = scanner.nextInt();
+        if (age >= 18) System.out.println("Jesteś pełnoletni.");
+        else System.out.println("Jesteś niepełnoletni.");
+    }
+}
