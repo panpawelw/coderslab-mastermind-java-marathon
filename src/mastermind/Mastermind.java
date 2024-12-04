8f25c02eb8570d559610a8b672f0c16ed1b1ec6d
Paweł Wojtuń <panpawelw@protonmail.com>
2024-12-05 00:17:37 +0100

    Add secret code generation

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,14 +1,28 @@
 package mastermind;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Mastermind {
+
+    public static final int CODE_LENGTH = 4;
+    public static final int MAX_DIGIT = 6;
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int[] secretCode = new int[CODE_LENGTH];
+
         System.out.println("Hello World!");
         System.out.println("Zgadnij 4-cyfrowy kod składający się z liczb od 1 do 6:");
-        Scanner scanner = new Scanner(System.in);
+
         String input = scanner.nextLine();
-        System.out.println(input);
+
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            secretCode[i] = random.nextInt(MAX_DIGIT) + 1;
+        }
+
         scanner.close();
     }
 }
