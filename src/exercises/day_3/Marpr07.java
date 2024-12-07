1c4da08fca58ba4d40961163b9337b4a97a58d04
Paweł Wojtuń <panpawelw@protonmail.com>
2024-12-08 00:18:02 +0100

    Day 3 exercise 1

---
+++ src/exercises/day_3/Marpr07.java
@@ -0,0 +1,12 @@
+package exercises.day_3;
+
+public class Marpr07 {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
