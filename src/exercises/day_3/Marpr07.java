ce4172a9245a58d47203d82d97465fc952f348ab
Paweł Wojtuń <panpawelw@protonmail.com>
2024-12-08 01:29:13 +0100

    Refactor day 3 exercise 1

--- src/exercises/day_3/Marpr07.java
+++ src/exercises/day_3/Marpr07.java
@@ -5,8 +5,8 @@ public class Marpr07 {
 
         int[] array = {1, 2, 3, 4, 5};
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        for (int j : array) {
+            System.out.println(j);
         }
     }
 }
