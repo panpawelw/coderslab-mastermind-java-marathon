bfca32f3a97653c2ee68a9c70dd31c37d62fd8b8
Paweł Wojtuń <panpawelw@protonmail.com>
2024-12-10 23:52:11 +0100

    Day 3 exercise 3

---
+++ src/exercises/day_3/Marpr09.java
@@ -0,0 +1,28 @@
+package exercises.day_3;
+
+import java.util.Random;
+
+public class Marpr09 {
+
+    public static boolean partOfArrayContains(int[] array, int index) {
+        for (int i = 0; i < index; i++) {
+            if (array[i] == array[index]) return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+        int[] array = new int[5];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            do {
+                array[i] = random.nextInt(50) + 1;
+            } while (partOfArrayContains(array, i));
+        }
+
+        for (int j : array) {
+            System.out.println(j);
+        }
+    }
+}
