7aeef3eac04d95effb8ed275119029abe198bf98
Paweł Wojtuń <panpawelw@protonmail.com>
2024-12-12 20:51:07 +0100

    Day 4 exercise 1

---
+++ src/exercises/day4/Marpr10.java
@@ -0,0 +1,17 @@
+package exercises.day4;
+
+import java.util.Scanner;
+
+public class Marpr10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę:");
+        int integer;
+        try {
+            integer = Integer.parseInt(scanner.nextLine());
+            System.out.println("Podałeś liczbę " + integer);
+        } catch (NumberFormatException e) {
+            System.out.println("To nie jest liczba!");
+        }
+    }
+}
