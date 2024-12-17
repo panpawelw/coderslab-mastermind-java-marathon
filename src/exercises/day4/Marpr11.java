d4fc0da8240b603113040133288705b6ad021726
Paweł Wojtuń <panpawelw@protonmail.com>
2024-12-17 19:57:36 +0100

    Change result type

--- src/exercises/day4/Marpr11.java
+++ src/exercises/day4/Marpr11.java
@@ -6,14 +6,14 @@ public class Marpr11 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj pierwszą liczbę całkowitą:");
+        System.out.print("Podaj pierwszą liczbę całkowitą: ");
         int firstNumber = scanner.nextInt();
-        System.out.println("Podaj drugą liczbę całkowitą:");
+        System.out.print("Podaj drugą liczbę całkowitą: ");
         int secondNumber = scanner.nextInt();
 
         try {
-            double result = (double) firstNumber / secondNumber;
-            System.out.println(result);
+            int result = firstNumber / secondNumber;
+            System.out.println("wynik wynosi: " + result);
 
         } catch(ArithmeticException e) {
             System.out.println("Dzielenie przez zero nie jest możliwe!");
