df9502a594ae20b5c82fb4df88cb233ce8522d94
Paweł Wojtuń <panpawelw@protonmail.com>
2025-03-22 21:14:51 +0100

    Refactor test

--- src/test/java/mastermind/MastermindTest.java
+++ src/test/java/mastermind/MastermindTest.java
@@ -8,14 +8,12 @@ public class MastermindTest {
 
     @Test
     public void testGenerateSecretCodeFillsArrayWithinRange() {
-        int[] code = new int[4]; // example size; adjust as needed
+        int[] code = new int[CODE_LENGTH];
         int[] secretCode = generateSecretCode(code);
-        // Assuming Mastermind.MAX_DIGIT is defined; if not, substitute a test value
-        int maxDigit = Mastermind.MAX_DIGIT;
 
         for (int digit : secretCode) {
             assertTrue(digit >= 0, "Digit should be at least 0");
-            assertTrue(digit < maxDigit, "Digit should be less than Mastermind.MAX_DIGIT (" + maxDigit + ")");
+            assertTrue(digit < MAX_DIGIT, "Digit should be less than Mastermind.MAX_DIGIT (" + MAX_DIGIT + ")");
         }
     }
 }
